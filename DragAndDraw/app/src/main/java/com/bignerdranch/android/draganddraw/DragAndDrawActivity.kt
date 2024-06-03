a0e82c500081fa7e6bce907eebbf18f8a4cf2345
Katesune <katesune.akk@gmail.com>
2024-06-03 13:43:33 +0300

    DragAndDraw: Complete tasks with repeating and reversing animation

--- DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.kt
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.kt
@@ -3,10 +3,12 @@ package com.bignerdranch.android.draganddraw
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.Animation
+import android.view.animation.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
@@ -28,6 +30,8 @@ class DragAndDrawActivity : AppCompatActivity() {
         ContextCompat.getColor(this, R.color.night_sky)
     }
 
+    private var sunsetReverse = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,34 +41,87 @@ class DragAndDrawActivity : AppCompatActivity() {
         skyView = findViewById(R.id.sky)
 
         sceneView.setOnClickListener {
-            startAnimation()
+            val currentAnimator = if (sunsetReverse) ReverseSunsetAnimator() else SunsetAnimator()
+            currentAnimator.startAnimation()
+            sunsetReverse = !sunsetReverse
         }
     }
 
-    private fun startAnimation() {
-        val sunYStart = sunView.top.toFloat()
-        val sunYEnd = skyView.height.toFloat()
+    abstract inner class Animation {
+        val sunYStart = sunView.y
+        val sunYEnd = skyView.bottom.toFloat() + 100
 
-        val heightAnimator = ObjectAnimator
-                .ofFloat(sunView, "y", sunYStart, sunYEnd)
-                .setDuration(3000)
+        abstract val yAnimator: ObjectAnimator
+        val sunsetAnimator = backGroundColorAnimator(sunsetSkyColor, 3000)
+        open val nightSkyAnimator = backGroundColorAnimator(nightSkyColor, 1500)
 
-        heightAnimator.interpolator = AccelerateInterpolator()
-
-        val sunsetSkyAnimator = ObjectAnimator
-                .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
-                .setDuration(3000)
-        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
-
-        val nightSkyAnimator = ObjectAnimator
-                .ofInt(skyView, "backgroundColor", sunsetSkyColor, nightSkyColor)
-                .setDuration(1500)
-        nightSkyAnimator.setEvaluator(ArgbEvaluator())
+        open val quakeAnimatorX = quakeAnimator("scaleX")
+        open val quakeAnimatorY = quakeAnimator("scaleY")
 
         val animatorSet = AnimatorSet()
-        animatorSet.play(heightAnimator)
-                .with(sunsetSkyAnimator)
-                .before(nightSkyAnimator)
-        animatorSet.start()
+
+        open fun startAnimation() {
+            animatorSet.play(yAnimator)
+                    .with(sunsetAnimator)
+                    .before(nightSkyAnimator)
+                    .with(quakeAnimatorX)
+                    .with(quakeAnimatorY)
+
+            animatorSet.start()
+        }
+    }
+
+    private inner class SunsetAnimator: Animation() {
+        override val yAnimator = yAnimator(sunYStart, sunYEnd)
+        override val nightSkyAnimator = backGroundColorAnimator(nightSkyColor, 1500)
+    }
+
+
+    private inner class ReverseSunsetAnimator: Animation() {
+        val canvasTop = (sceneView.height / 6).toFloat()
+
+        override val yAnimator = yAnimator(sunYStart, canvasTop)
+        override val nightSkyAnimator = backGroundColorAnimator(blueSkyColor, 1000)
+
+        override fun startAnimation() {
+            super.animatorSet.play(yAnimator)
+                    .with(sunsetAnimator)
+                    .before(nightSkyAnimator)
+                    .with(quakeAnimatorX)
+                    .with(quakeAnimatorY)
+
+            animatorSet.start()
+        }
+    }
+
+    val yAnimator: (Float, Float) -> ObjectAnimator = { yStart, yEnd ->
+        val animator = ObjectAnimator
+                .ofFloat(sunView, "y", yStart, yEnd)
+                .setDuration(3000)
+
+        animator.interpolator = AccelerateInterpolator()
+        animator
+    }
+
+    val backGroundColorAnimator: (Int, Long) -> ObjectAnimator = { colorEnd, duration ->
+        val backgroundColor = (skyView.background as? ColorDrawable)?.color ?: 0
+        skyView.setBackgroundColor(colorEnd)
+
+        val animator = ObjectAnimator
+                .ofInt(skyView, "backgroundColor", backgroundColor, colorEnd)
+                .setDuration(duration)
+
+        animator.setEvaluator(ArgbEvaluator())
+        animator
+    }
+
+    val quakeAnimator: (String) -> ObjectAnimator = { propertyName ->
+        val quakeAnimator = ObjectAnimator
+                .ofFloat(sunView, propertyName, 1F, 1.03F)
+                .setDuration(200)
+
+        quakeAnimator.repeatCount = 30
+        quakeAnimator.interpolator = BounceInterpolator()
+        quakeAnimator
     }
 }
